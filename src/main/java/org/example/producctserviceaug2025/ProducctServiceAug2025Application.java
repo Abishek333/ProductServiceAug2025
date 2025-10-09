96b7b59a015b167e3c75e99cfe85ffcb01f0ccc2
Abish <mailtoabi333@gmail.com>
2025-10-09 23:30:45 +0530

    F commit

---
+++ src/main/java/org/example/producctserviceaug2025/ProducctServiceAug2025Application.java
@@ -0,0 +1,13 @@
+package org.example.producctserviceaug2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducctServiceAug2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducctServiceAug2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/producctserviceaug2025/ProducctServiceAug2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.producctserviceaug2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducctServiceAug2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
